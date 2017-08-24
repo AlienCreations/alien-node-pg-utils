d861163c0d84b21090e926298522cf31b9afda36
Sean Cannon <alienwebguy@gmail.com>
2017-08-24 12:55:44 -0700

    1.1.1 - refactor prepared statement syntax for inserts and sets

--- spec/prepareProvidedFieldsForInsertSpec.js
+++ spec/prepareProvidedFieldsForInsertSpec.js
@@ -4,6 +4,6 @@ const prepareProvidedFieldsForInsert = require('../lib/methods/prepareProvidedFi
 
 describe('prepareProvidedFieldsForInsert', () => {
   it('makes a prepared statement for each field, sorted alphabetically by column name', () => {
-    expect(prepareProvidedFieldsForInsert(['foo', 'bar'])).toBe('("foo","bar")');
+    expect(prepareProvidedFieldsForInsert(['foo', 'bar'])).toBe('(foo, bar) VALUES ($1, $2)');
   });
 });
